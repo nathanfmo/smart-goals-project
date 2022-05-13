2392250238cd1e3984906125dddee44069ce781d
Nathan M <78625898+nathanfmo@users.noreply.github.com>
2022-05-13 00:25:59 +0000

    Finished Relevant and Time-Bound, finished final output

--- main.sh
+++ main.sh
@@ -30,23 +30,43 @@ read -p "And how often will you be $action (daily, weekly, three times, etc.)? I
 echo ""
 
 #Achievable
-echo "Now for a reality check- is this goal achievable? Be honest with yourself, because aiming too high will be discouraging. Starting small and building up is always the best way to go!"
+echo "Now for a reality check- is this action achievable? Be honest with yourself, because aiming too high will be discouraging. Starting small and building up is always the best way to go!"
 echo ""
 echo "Your goal is to $goal by $action $interval"
 echo ""
 read -p "Is this achievable? 1 for yes or 2 for no: " achievable
+echo ""
 if [ "$achievable" == "2" ]
 then
-  echo "That's okay, let's try again!"
-  echo ""
-  echo ""
-  bash main.sh
+  echo "That's okay, let's change your action!"
+  read -p "I will achieve my goal by " action
+  read -p "I will do this (daily, weekly, three times, etc.) " interval
 elif [ "$achievable" != "1" ]
 then  
   echo "I'm going to assume you meant 1. Moving right along!"
 fi
+echo ""
 
 #Relevant
-
+read -p "Now we want to make sure that this action is relevant to your values. Does this action reflect those values and move you towards your bigger goals? 1 for yes or 2 for no: " relevant
+echo ""
+if [ "$relevant" == "2" ]
+then
+  echo "That's okay, let's try again with your values and bigger goals in mind!"
+  echo ""
+  echo ""
+  bash main.sh
+elif [ "$relevant" != "1" ]
+then  
+  echo "I'm going to assume you meant 1. Moving right along!"
+fi
+echo ""
 
 #Time-Bound
+echo "There's a saying that a goal with no deadline with never be done, let's make sure that's not the case! Think of when you want to have this goal done by."
+echo ""
+read -p "I will achieve $goal by (next month, next year, the end of the semester, etc.) " deadline
+echo ""
+echo ""
+echo "Alright, here's your new SMART goal! Good luck!"
+echo "By $deadline, I will $goal. I will do this by $action $interval."
